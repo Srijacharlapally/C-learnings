f31f0286cfafa7b9d6069ab6ccb59866efc79697
Srijacharlapally <77089988+Srijacharlapally@users.noreply.github.com>
2021-02-04 16:21:36 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace myproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Num = 30.33;
+            Console.WriteLine(Num);  
+        }
+    }
+}
