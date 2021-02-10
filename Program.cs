5be9f3a2b62aafabf261b81f86297057dbdd6d22
Srijacharlapally <77089988+Srijacharlapally@users.noreply.github.com>
2021-02-10 10:11:29 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -3,22 +3,14 @@
 namespace myproject
 {
     class Program
-    {
+    {           
         static void Main(string[] args)
         {
-         int a = 90;
-         char s = 'c';
-         float d = 20.1f; 
-         double num = 30.020;
-         string bol = "srija"; 
-         Boolean name = true;
-    
-         Console.WriteLine(a);
-         Console.WriteLine(s);
-         Console.WriteLine(d);
-         Console.WriteLine(num);
-         Console.WriteLine(bol);
-         Console.WriteLine(name);
+         /*string message = "I want to see Red movie";
+
+         Console.WriteLine(message);*/
+         charlapally amazon = new charlapally();
+         Console.WriteLine(amazon.addition(6,11));
            
         }
     }
