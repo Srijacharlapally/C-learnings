eea4f56587059c5cc896b089aa17f309002e71f8
Srijacharlapally <77089988+Srijacharlapally@users.noreply.github.com>
2021-03-10 18:13:54 +0530

    Add files via upload

---
+++ enum.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace webex
+{
+    class multicolors 
+    {
+        // enum mycolors 
+        // {
+        //     Red=1;
+        //     blue=2;
+        //     white=3;
+        //     black=4;
+        //     yellow=5;
+        //     {
+        //         static void main(string[]args)
+        //         {
+        //             else if (mycolor==1)
+        //             {
+        //                 console.writeLine("Red");
+        //             }
+        //             else if (mycolor==2)
+        //             {
+        //                 console.writeLine("blue");
+        //             }
+        //             else if (mycolors==3)
+        //             {
+        //                 Console.writeLine("white");
+        //             }
+        //             else if (mycolors==4)
+        //             {
+        //                 Console.WriteLine("black");
+        //             }
+        //             else if (mycolors==5)
+        //             {
+        //                 Console.WriteLine("yellow");
+        //             }
+        //         }
+
+        //     }
+        // }
+    }
+}
